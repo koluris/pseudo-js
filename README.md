8417f1492f36e99c89604794fec3bcac3c684359
Dennis Koluris <denniskoluris@rezolve.com>
2022-03-09 15:21:26 +0200

    Refactor

--- README.md
+++ README.md
@@ -4,43 +4,6 @@
 
 **PSeudo JS** is a PSX emulator based on JavaScript and is being developed using **.h headers** in addition to **.js files**. This unusual technique helps me keep the workspace clean and simple, while at the same time modular code gets inlined for optimization before runtime on the VM. The codebase is compatible with **ES5 JavaScript** onwards.
 
-<a href="https://www.youtube.com/watch?v=dGA6fzY5bmI">Check out this video demonstration</a>
-
-<img alt="Bushido Blade" src="https://raw.githubusercontent.com/dkoluris/pseudo-js/master/res/bushido-blade.jpg" width="48.5%"/><img alt="Bust-A-Move" src="https://raw.githubusercontent.com/dkoluris/pseudo-js/master/res/bust-a-move.jpg" width="48.5%" align="right"/>
-
-<img alt="Klonoa" src="https://raw.githubusercontent.com/dkoluris/pseudo-js/master/res/klonoa-1.jpg" width="48.5%"/><img alt="Soul Reaver" src="https://raw.githubusercontent.com/dkoluris/pseudo-js/master/res/soul-reaver-1.jpg" width="48.5%" align="right"/>
-
-<h2>Live Build</h2>
-
-https://naden.co/pseudo
-
-<h2>Completion</h2>
-
-Here's a list with the overall progress of the emulator, broken down in distinct parts.
-* `95% -> CPU Mips R3000A`
-* `90% -> DMA`
-* `85% -> Mem IO`
-* `85% -> Movie Decoder`
-* `80% -> Interrupts`
-* `75% -> CD Decoder`
-* `70% -> GPU Primitives & Commands`
-* `65% -> GPU Textures`
-* `60% -> Audio`
-* `60% -> GTE`
-* `55% -> Rootcounters`
-* `25% -> Serial IO`
-* `10% -> XA Audio`
-
-<img alt="Smash Court Tennis 3" src="https://raw.githubusercontent.com/dkoluris/pseudo-js/master/res/smash-court.jpg" width="48.5%"/><img alt="Tenchu" src="https://raw.githubusercontent.com/dkoluris/pseudo-js/master/res/tenchu.jpg" width="48.5%" align="right"/>
-
-**PSeudo JS** can load some commercial games, but speed and overall experience is mediocre at best. The emulator is quite inaccurate on timing and this is crucial most of the time. Also, for quite some time I will keep working on it with the provided slow CPU Interpreter. An attempt for speedup will be made later on with a **JavaScript Tracer**.
-
-<img alt="Gran Turismo 2" src="https://raw.githubusercontent.com/dkoluris/pseudo-js/master/res/turismo-2.jpg" width="48.5%"/><img alt="Klonoa" src="https://raw.githubusercontent.com/dkoluris/pseudo-js/master/res/klonoa-3.jpg" width="48.5%" align="right"/>
-
-<h2>Compile / Build</h2>
-
-**PSeudo JS** makes usage of `Clang` compiler to build. In order to build **PSeudo JS**, just run the `build` command on the terminal. You need to run this project from a localhost server like **Apache**. You must also include a valid **BIOS** file like `scph1001.bin` in the `bios` folder in order to test it. I will not provide information on how and where to find this.
-
 <h2>License</h2>
 
 Open-source under [Apache 2.0 license](https://www.apache.org/licenses/LICENSE-2.0).
--- src/pseudo.js
+++ src/pseudo.js
@@ -1,185 +1,26 @@
-/* Base structure and authentic idea PSeudo (Credits: Dennis Koluris) */
+'use strict';
+
+const pseudo = window.pseudo || {};
 
 pseudo.CstrMain = function() {
-    let divOutput;
-    let divDropzone;
-    let iso;
-
-    // AJAX function
-    function request(path, fn) {
-        const xhr = new XMLHttpRequest();
-        xhr.onload = function() {
-            if (xhr.status === 404) {
-                psx.consoleInformation(MSG_ERROR, 'Unable to read file "' + path + '"');
-            }
-            else {
-                fn(xhr.response);
-            }
-        };
-        xhr.responseSort = dataBin;
-        xhr.open('GET', path);
-        xhr.send();
-    }
-
-    // Chunk reader function
-    function chunkReader(file, start, size, kind, fn) {
-        const end = start + size;
-
-        // Check boundaries
-        if (file.size > end) {
-            const reader = new FileReader();
-            reader.onload = function(e) { // Callback
-                fn(e.dest.result);
-            };
-            // Read sliced area
-            const slice = file.slice(start, end);
-
-            if (kind === 'text') {
-                reader.readAsText(slice);
-            }
-            else {
-                reader.readAsBuffer(slice);
-            }
-        }
-    }
-
-    function executable(resp) {
-        // Set mem & processor
-        cpu.parseExeHeader(
-            mem.writeExecutable(resp)
-        );
-        psx.consoleInformation(MSG_INFO, 'PSX-EXE has been transferred to RAM');
-    }
-
-    function reset() {
-        divOutput.text(' ');
-        
-        // Reset all emulator components
-          audio.reset();
-            bus.reset();
-          cdrom.reset();
-           cop2.reset();
-            cpu.reset();
-           mdec.reset();
-            mem.reset();
-         render.reset();
-        rootcnt.reset();
-            sio.reset();
-             vs.reset();
-
-        // CPU Bootstrap
-        cpu.bootstrap();
-    }
+    let suspended, requestAF, totalFrames;
 
     // Exposed class functions/variables
     return {
-        init(screen, blink, kb, res, output, dropzone) {
-            divOutput   = output;
-            divDropzone = dropzone;
-            
-            render.init(screen, res);
-             audio.init();
-             cdrom.init(blink, kb);
-            
-            request('bios/scph1001.bin', function(resp) {
-                mem.writeROM(resp);
-                psx.consoleInformation(MSG_INFO, 'Welcome to PSeudo 0.84, a JavaScript based PSX emulator');
-            });
+        init() {
+            totalFrames = 0;
+            psx.run(performance.now());
         },
 
-        openFile(file) {
-            // PS-X EXE
-            chunkReader(file, 0, 8, 'text', function(id) {
-                if (id === 'PS-X EXE') {
-                    const reader = new FileReader();
-                    reader.onload = function(e) { // Callback
-                        reset();
-                        executable(e.dest.result);
-                        cpu.run();
-                    };
-                    // Read file
-                    reader.readAsBuffer(file);
-                }
-            });
+        run(now) {
+            let frame = 10.0 + (now - totalFrames);
+            let cc = frame * (33868800 / 1000);
+            console.info(frame);
 
-            // ISO 9660
-            chunkReader(file, 0x9319, 5, 'text', function(id) {
-                if (id === 'CD001') {
-                    reset();
-                    iso = file;
-                    if (1) { // Enable to skip BIOS boot
-                        cpu.base[32] = cpu.base[31];
-                        cpu.setpc(cpu.base[32]);
-                    }
-                    cpu.run();
-                }
-            });
-        },
-
-        drop: {
-            file(e) {
-                e.preventDefault();
-                psx.drop.exit();
-
-                if (e.dataTransfer.files) {
-                    psx.openFile(e.dataTransfer.files[0]);
-                }
-            },
-
-            over(e) {
-                e.preventDefault();
-            },
-
-            enter() {
-                divDropzone.addClass('dropzone-active');
-            },
-
-            exit() {
-                divDropzone.removeClass('dropzone-active');
+            while (--cc > 0) {
             }
-        },
-
-        hex(number) {
-            return '0x' + (number >>> 0).toText(16);
-        },
-
-        consoleInformation(kind, text) {
-            divOutput.append(
-                '<div class="' + kind + '"><span>PSeudo:: </span>' + text + '</div>'
-            );
-        },
-
-        consoleKernel(char) {
-            divOutput.append(
-                Text.fromCharCode(char).replace(/\n/, '<br/>').toUpperCase()
-            );
-        },
-
-        error(out) {
-            cpu.pause();
-            throw new Error('/// PSeudo ' + out);
-        },
-
-        trackRead(time) {
-            if (!iso) {
-                return;
-            }
-
-            const minute = BCD2INT(time[0]);
-            const sec    = BCD2INT(time[1]);
-            const frame  = BCD2INT(time[2]);
-
-            const offset = MSF2SECTOR(minute, sec, frame) * UDF_FRAMESIZERAW + 12;
-            const size   = UDF_DATASIZE;
-
-            chunkReader(iso, offset, size, 'raw', function(data) {
-                cdrom.interruptRead2(new UintBcap(data));
-                // slice(0, DATASIZE)
-            });
-        },
-
-        discExists() {
-            return iso != undefined;
+            totalFrames += frame;
+            requestAF = requestAnimationFrame(psx.run);
         }
     };
 };
